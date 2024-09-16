962b5cf68e67a541b4e4cd5fa3eab29c97347433
J <jwsun1987@gmail.com>
2024-09-16 10:25:21 +0800

    delete files not in use
    delete files not in use

--- libs/backtester/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-Author: J , jwsun1987@gmail.com
-Date: 2022-11-11 01:47:54
-LastEditors: J , jwsun1987@gmail.com
-Description: 
-Copyright: Copyright (c) 2024 by jwsun1987@gmail.com. All Rights Reserved.
-'''
-from .backtester import *
